79cbbe837cd7c326d3ea1153e206d10d19124914
CS <claudia.j.sm@gmail.com>
2018-08-18 18:48:05 -0500

    test GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const { ObjectID } = require('mongodb');
 
 const { app } = require('./../server')
 const { Todo } = require('./../models/todo');
 
 const todos = [{
+    _id: new ObjectID(),
     text: 'first test todo'
 }, {
+    _id: new ObjectID(),
     text: 'second test todo'
 }];
 
@@ -76,4 +79,32 @@ describe('POST /todos', () => {
     }, (e) => {
 
     });
+});
+
+describe('GET /todos/:id', () => {
+    it('should return todo doc', (done) => {
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done);
+    }, (e) => {
+    })
+
+    it('should return 404 if todo not found', (done) => {
+        var newID = new ObjectID();
+        request(app)
+            .get(`/todos/${newID.toHexString()}`)
+            .expect(404)
+            .end(done);
+    }, (e) => { });
+
+    it('should return 404 for non-object ids', (done) => {
+        request(app)
+            .get('/todos/123')
+            .expect(404)
+            .end(done);
+    }, (e) => { });
 });

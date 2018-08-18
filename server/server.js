eb0cd88fd467b1639f46312a6d8c1e5cc3bf03bd
CS <claudia.j.sm@gmail.com>
2018-08-18 18:00:54 -0500

    add GET /todos route

--- server/server.js
+++ server/server.js
@@ -9,6 +9,14 @@ var app = express();
 
 app.use(bodyParser.json());
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos });
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
@@ -25,4 +33,4 @@ app.listen(3000, () => {
     console.log('Started on port 3000');
 })
 
-module.exports = {app};
+module.exports = { app };
